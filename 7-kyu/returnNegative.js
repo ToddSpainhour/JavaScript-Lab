1d2e64b7201fe9bc5c769282d3d282cad37d64bb
Todd Spainhour <toddspainhour@gmail.com>
2021-05-07 19:33:51 -0500

    return negative challenge complete

---
+++ 7-kyu/returnNegative.js
@@ -0,0 +1,20 @@
+/*
+https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+*/
+
+const makeNegative = (number) => {
+  if (number < 0) {
+    
+    return number
+    
+  } else {
+    
+    const nmakeNumberIsNegativeButAString = '-' + Math.abs(number);
+    
+    const convertedStringToNumber = parseFloat(nmakeNumberIsNegativeButAString);
+    
+      return convertedStringToNumber;
+  }
+}
